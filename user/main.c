881967dc85f3663b8588c0c9923e78309f24d075
TJY <641533882@qq.com>
2016-06-11 22:04:36 +0800

    添加串口DMA发送数据

--- user/main.c
+++ user/main.c
@@ -10,6 +10,41 @@
 #define TASK_LCD_STACK_SIZE configMINIMAL_STACK_SIZE			//任务堆栈大小
 #define TASK_INIT_STACK_SIZE configMINIMAL_STACK_SIZE			//任务堆栈大小
 #define TASK_COMM_STACK_SIZE configMINIMAL_STACK_SIZE			//任务堆栈大小
+
+#define USARTx                           USART6
+#define USARTx_CLK_ENABLE()              __USART6_CLK_ENABLE()
+#define DMAx_CLK_ENABLE()                __HAL_RCC_DMA2_CLK_ENABLE()
+#define USARTx_RX_GPIO_CLK_ENABLE()      __GPIOC_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __GPIOC_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __USART6_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __USART6_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_6
+#define USARTx_TX_GPIO_PORT              GPIOC
+#define USARTx_TX_AF                     GPIO_AF8_USART6
+#define USARTx_RX_PIN                    GPIO_PIN_7
+#define USARTx_RX_GPIO_PORT              GPIOC
+#define USARTx_RX_AF                     GPIO_AF8_USART6
+
+/* Definition for USARTx's DMA */
+#define USARTx_TX_DMA_STREAM              DMA2_Stream6
+#define USARTx_RX_DMA_STREAM              DMA2_Stream1
+#define USARTx_TX_DMA_CHANNEL             DMA_CHANNEL_5
+#define USARTx_RX_DMA_CHANNEL             DMA_CHANNEL_5
+
+
+/* Definition for USARTx's NVIC */
+#define USARTx_DMA_TX_IRQn                DMA2_Stream6_IRQn
+#define USARTx_DMA_RX_IRQn                DMA2_Stream1_IRQn
+#define USARTx_DMA_TX_IRQHandler          DMA2_Stream6_IRQHandler
+#define USARTx_DMA_RX_IRQHandler          DMA2_Stream1_IRQHandler
+
+/* Definition for USARTx's NVIC */
+#define USARTx_IRQn                      USART6_IRQn
+#define USARTx_IRQHandler                USART6_IRQHandler
+UART_HandleTypeDef UartHandle;
 enum
 {
 	TASK_LED_PRI = 1,
@@ -61,8 +96,102 @@ void Task_INIT(void *p)
 }
 void Task_COMM(void *p)
 {
+	GPIO_InitTypeDef  GPIO_InitStruct;
+	
+	static DMA_HandleTypeDef hdma_tx;
+  static DMA_HandleTypeDef hdma_rx;
+	/* Buffer used for transmission */
+	uint8_t TxBuffer[] = "what the fuck";
+	
+  /*##-1- Enable peripherals and GPIO Clocks #################################*/
+  /* Enable GPIO TX/RX clock */
+  USARTx_TX_GPIO_CLK_ENABLE();
+  USARTx_RX_GPIO_CLK_ENABLE();
+
+
+  /* Enable USARTx clock */
+  USARTx_CLK_ENABLE(); 
+  
+	/* Enable DMA clock */
+  DMAx_CLK_ENABLE();
+	
+  /*##-2- Configure peripheral GPIO ##########################################*/  
+  /* UART TX GPIO pin configuration  */
+  GPIO_InitStruct.Pin       = USARTx_TX_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_PULLUP;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+  GPIO_InitStruct.Alternate = USARTx_TX_AF;
+
+  HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+
+  /* UART RX GPIO pin configuration  */
+  GPIO_InitStruct.Pin = USARTx_RX_PIN;
+  GPIO_InitStruct.Alternate = USARTx_RX_AF;
+
+  HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+	
+	/*##-3- Configure the DMA ##################################################*/
+  /* Configure the DMA handler for Transmission process */
+  hdma_tx.Instance                 = USARTx_TX_DMA_STREAM;
+  hdma_tx.Init.Channel             = USARTx_TX_DMA_CHANNEL;
+  hdma_tx.Init.Direction           = DMA_MEMORY_TO_PERIPH;
+  hdma_tx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_tx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_tx.Init.MemDataAlignment    = DMA_MDATAALIGN_BYTE;
+  hdma_tx.Init.Mode                = DMA_NORMAL;
+  hdma_tx.Init.Priority            = DMA_PRIORITY_LOW;
+
+  HAL_DMA_Init(&hdma_tx);
+
+  /* Associate the initialized DMA handle to the UART handle */
+  __HAL_LINKDMA(&UartHandle, hdmatx, hdma_tx);
+
+  /* Configure the DMA handler for reception process */
+  hdma_rx.Instance                 = USARTx_RX_DMA_STREAM;
+  hdma_rx.Init.Channel             = USARTx_RX_DMA_CHANNEL;
+  hdma_rx.Init.Direction           = DMA_PERIPH_TO_MEMORY;
+  hdma_rx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_rx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_rx.Init.MemDataAlignment    = DMA_MDATAALIGN_BYTE;
+  hdma_rx.Init.Mode                = DMA_NORMAL;
+  hdma_rx.Init.Priority            = DMA_PRIORITY_HIGH;
+
+  HAL_DMA_Init(&hdma_rx);
+
+  /* Associate the initialized DMA handle to the the UART handle */
+  __HAL_LINKDMA(&UartHandle, hdmarx, hdma_rx);
+    
+  /*##-4- Configure the NVIC for DMA #########################################*/
+  /* NVIC configuration for DMA transfer complete interrupt (USART6_TX) */
+  HAL_NVIC_SetPriority(USARTx_DMA_TX_IRQn, 0, 1);
+  HAL_NVIC_EnableIRQ(USARTx_DMA_TX_IRQn);
+    
+  /* NVIC configuration for DMA transfer complete interrupt (USART6_RX) */
+  HAL_NVIC_SetPriority(USARTx_DMA_RX_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USARTx_DMA_RX_IRQn);
+  
+  /* NVIC for USART, to catch the TX complete */
+  HAL_NVIC_SetPriority(USARTx_IRQn, 0, 1);
+  HAL_NVIC_EnableIRQ(USARTx_IRQn);
+	
+	UartHandle.Instance        = USARTx;
+	UartHandle.Init.BaudRate = 115200;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+  UartHandle.Init.StopBits   = UART_STOPBITS_1;
+  UartHandle.Init.Parity     = UART_PARITY_NONE;
+  UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+  UartHandle.Init.Mode       = UART_MODE_TX_RX;
+  UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	HAL_UART_DeInit(&UartHandle);
+	HAL_UART_Init(&UartHandle);
+	
 	for(;;)
 	{
+		HAL_UART_Transmit_DMA(&UartHandle, (uint8_t*)TxBuffer, sizeof(TxBuffer) / sizeof(*(TxBuffer)));
+		//HAL_UART_Transmit(&UartHandle, (uint8_t*)TxBuffer, sizeof(TxBuffer) / sizeof(*(TxBuffer)), 5000);
 		vTaskDelay(1000);
 	}
 }
@@ -84,7 +213,7 @@ void Task_LCD(void *p)
   BSP_LCD_SetTextColor(LCD_COLOR_DARKBLUE);
 
   /* Display LCD messages */
-  BSP_LCD_DisplayStringAt(0, 10, (uint8_t *)"STM32F746G LCD Demo", CENTER_MODE);
+  BSP_LCD_DisplayStringAt(0, 10, (uint8_t *)"STM32F746G FreeRTOS Demo", CENTER_MODE);
   BSP_LCD_DisplayStringAt(0, 35, (uint8_t *)"What the fuck !!!", CENTER_MODE);
 	vTaskDelete(NULL);
 }
@@ -183,5 +312,41 @@ void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
 void vApplicationIdleHook (void)
 {
 
+}
+/**
+  * @brief  This function handles DMA interrupt request.  
+  * @param  None
+  * @retval None
+  * @Note   This function is redefined in "main.h" and related to DMA  
+  *         used for USART data transmission     
+  */
+void USARTx_DMA_RX_IRQHandler(void)
+{
+  HAL_DMA_IRQHandler(UartHandle.hdmarx);
+}
+
+/**
+  * @brief  This function handles DMA interrupt request.
+  * @param  None
+  * @retval None
+  * @Note   This function is redefined in "main.h" and related to DMA  
+  *         used for USART data reception    
+  */
+void USARTx_DMA_TX_IRQHandler(void)
+{
+  HAL_DMA_IRQHandler(UartHandle.hdmatx);
+}
+
+
+/**
+  * @brief  This function handles UART interrupt request.  
+  * @param  None
+  * @retval None
+  * @Note   This function is redefined in "main.h" and related to DMA  
+  *         used for USART data transmission     
+  */
+void USARTx_IRQHandler(void)
+{
+  HAL_UART_IRQHandler(&UartHandle);
 }
 
